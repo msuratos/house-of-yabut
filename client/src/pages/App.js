cdda7de10f5d29809b8572d9bd3358b0ab4be1c4
Melvin Suratos <melvinsuratos@hotmail.com>
2021-05-16 16:09:12 -0700

    Added details page that shows youtube video

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Countdown from '../components/Countdown';
 import SnowBackground from '../components/SnowBackground';
+import Details from './Details';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SnowBackground />
-      <Countdown />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <div className="App">
+            <SnowBackground />
+            <Countdown />
+          </div>
+        </Route>
+        <Route exact path='/details'>
+          <Details />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Details.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Details = () => {
+    return (
+        <>
+            <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/lJJCzK0eFII?autoplay=1&fs=1&playsinline=0" 
+                title="YouTube video player" frameborder="0" allowfullscreen
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        </>
+    );
+};
+
+export default Details;
