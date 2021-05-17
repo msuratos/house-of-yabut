135cdae92cb4f5c7920febe3db388bcf9211a5e2
Melvin Suratos <melvinsuratos@hotmail.com>
2021-05-16 21:36:14 -0700

    Created new video component & used YouTube IFrame Api - [x] Used IFrame Api to create embedded YouTube video - [x] Included a state to only show details once video has finished playing

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -1,43 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Video from '../components/Video';
 import './Details.css'
 
 const Details = () => {
-    const onLocationClick = () => {
-        if (
-            (navigator.platform.indexOf("iPhone") !== -1) || 
-            (navigator.platform.indexOf("iPad") !== -1) || 
-            (navigator.platform.indexOf("iPod") !== -1)
-        )
-            window.open("https://maps.apple.com/maps?daddr=9721+Pan+Falls+Dr,+Las+Vegas,+NV+89149");
-        else /* else use Google */
-            window.open("https://goo.gl/maps/YToZSDL56WfefREy9");
-    };
+  const [showcontent, setshowcontent] = useState(false);
 
-    return (
-        <>
-            <div className="details-container">
-                <iframe height="30%" width="100%" src="https://www.youtube-nocookie.com/embed/lJJCzK0eFII?autoplay=1&fs=1&playsinline=0" 
-                    title="YouTube video player" frameBorder="0" allowFullScreen
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-                <div className="details-content">
-                    <p>You have been cordially invited to attend the GoT gender reveal party</p>
-                    <ul>
-                        <li>
-                            <label className="details-content-label">Location:</label>
-                            <a href="/details" onClick={onLocationClick}>9721 Pine Falls Street Las Vegas, NV 89178</a>
-                        </li>
-                        <li>
-                            <label className="details-content-label">Facebook Live:</label>
-                            <a href="/details" onClick={(e) => window.open("https://www.facebook.com/events/129180445848737")}>
-                                Facebook Live Link (For those unable to grace us with their presence
-                            </a>
-                        </li>
-                    </ul>
-                    <p>Answer to riddle: Put invitation in fridge for 15-30min</p>
-                </div>
-            </div>
-        </>
-    );
+  const onLocationClick = () => {
+    if (
+      (navigator.platform.indexOf("iPhone") !== -1) ||
+      (navigator.platform.indexOf("iPad") !== -1) ||
+      (navigator.platform.indexOf("iPod") !== -1)
+    )
+      window.open("https://maps.apple.com/maps?daddr=9721+Pan+Falls+Dr,+Las+Vegas,+NV+89149");
+    else /* else use Google */
+      window.open("https://goo.gl/maps/YToZSDL56WfefREy9");
+  };
+
+  return (
+    <>
+      <div className="details-container">
+        <Video id="lJJCzK0eFII" setshowcontent={setshowcontent} />
+        {showcontent && (
+          <div className="details-content">
+            <p>You have been cordially invited to attend the GoT gender reveal party</p>
+            <ul>
+              <li>
+                <label className="details-content-label">Location:</label>
+                <a href="/details" onClick={onLocationClick}>9721 Pine Falls Street Las Vegas, NV 89178</a>
+              </li>
+              <li>
+                <label className="details-content-label">Facebook Live:</label>
+                <a href="/details" onClick={(e) => window.open("https://www.facebook.com/events/129180445848737")}>
+                  Facebook Live Link (For those unable to grace us with their presence
+                </a>
+              </li>
+            </ul>
+            <p>Answer to riddle: Put invitation in fridge for 15-30min</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Details;
