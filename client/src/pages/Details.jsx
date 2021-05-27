b0f1f6a3318d5c298a0035f0b99dee74d9055245
Melvin Suratos <72821450+msuratos@users.noreply.github.com>
2021-05-26 19:59:14 -0700

    Updated the details information (#24)

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -20,7 +20,7 @@ const Details = () => {
   return (
     <>
       <div className="details-container">
-        <Video id="lJJCzK0eFII" setshowcontent={setshowcontent} />
+        <Video id="Muako8SBJ7c" setshowcontent={setshowcontent} />
         <div style={{height: '100%', width: '100%', position: 'fixed'}}>
           <div className="details-content-background"></div>
           {showcontent && (
@@ -29,16 +29,22 @@ const Details = () => {
                 <p style={{textAlign: 'center'}}>You have been cordially invited to attend the Game of Thrones gender reveal party</p>
                 <ul>
                   <li>
+                    <label className="details-content-label">When:</label>
+                    <span style={{fontSize: 'x-small'}}>June <span className="got-number">5</span>th &nbsp;
+                    <span className="got-number">2021</span> at <span className="got-number">4</span>pm</span>
+                  </li>
+                  <li>
+                    <label className="details-content-label">Where:</label>
+                    <a href="/details" onClick={onLocationClick}>
+                      <span className="got-number">9721</span> Pan Falls &nbsp;St. &nbsp;Las Vegas, NV <span className="got-number">89178</span>
+                    </a>
+                  </li>
+                  <li>&nbsp;</li>
+                  <li>
                     <div style={{paddingBottom: '5px'}}><span style={{fontSize: 'xx-small'}}>For those unable to grace us with their presence,</span></div>
                     <label className="details-content-label">Facebook Live:</label>
                     <a href="/details" onClick={(e) => window.open("https://fb.me/e/2iHXYMCHd")}>
                       Click Here
-                    </a>
-                  </li>
-                  <li>
-                    <label className="details-content-label">Address:</label>
-                    <a href="/details" onClick={onLocationClick}>
-                      <span className="got-number">9721</span> Pan Falls &nbsp;St. &nbsp;Las Vegas, NV <span className="got-number">89178</span>
                     </a>
                   </li>
                 </ul>
